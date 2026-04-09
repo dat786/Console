8c3bfa7ed0f13aeec6939386b63aeff34c88be7b
dat786 <power.aidan@icloud.com>
2026-04-09 16:30:42 +0100

    root

--- apiserverdata.js
+++ apiserverdata.js
@@ -1,23 +1,14 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
+const fs = require('fs');
+const path = require('path');
 
-  res.status(200).json({
-    "menu-version": "1.0.0",
-    "min-version": "1.0.0",
-    "min-CXS-version": "1.0.0",
-    "motd": "Welcome to untitledV2 Join our Discord for support and updates.",
-    "discord-invite": "https://discord.gg/z6K8DGYCA6",
-    "admins": [
-      { "name": "imudtrust4", "user-id": "9157B6874B6C884D" },
-      { "name": "imudtrust3", "user-id": "B4924E637C16EAF" }
-    ],
-    "super-admins": [
-      "imudtrust3",
-      "imudtrust2",
-      "imudtrust"
-    ],
-    "blacklisted-ids": [
-      "B00C644BE8902563"
-    ]
-  });
-}
+module.exports = (req, res) => {
+  // Find the HTML file
+  const filePath = path.join(process.cwd(), 'api', 'data.html');
+  
+  // Read the HTML file
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+
+  // Send it to the browser
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(fileContent);
+};
